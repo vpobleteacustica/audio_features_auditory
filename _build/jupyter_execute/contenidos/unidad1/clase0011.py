11036b9bbf4684f19d0a474b3631ba4c632d4022
= <=>
2022-06-22 19:23:17 -0400

    adding changes to audio feature book!

---
+++ _build/jupyter_execute/contenidos/unidad1/clase0011.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython.display import Audio
+
+
+# # Sistema auditivo periférico
+
+# In[2]:
+
+
+from IPython.display import YouTubeVideo
+YouTubeVideo("eQEaiZ2j9oc")
+
+
+# # En acústica existe `Resonancia`
+
+# ## Una descripción del concepto a través del:
+# 
+# > Tubo de Rubens.
+
+# In[3]:
+
+
+from IPython.display import YouTubeVideo
+YouTubeVideo("dihQuwrf9yQ")
+
+
+# In[ ]:
+
+
+
+
